dbc34390f22e6d3dd4433fe07ffaec6622722a17
Brendon <hall.brendonp@gmail.com>
2023-11-26 14:07:22 -0600

    Add Admin1

--- HallConnections/CRM/models.py
+++ HallConnections/CRM/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import models as authModels
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    created_on = models.DateField(auto_now_add=True)
+
+class Change(models.Model):
+    user = models.ForeignKey(authModels.User, on_delete=models.RESTRICT)
+    created_on = models.DateTimeField(auto_created=True)
+
+
+
+
