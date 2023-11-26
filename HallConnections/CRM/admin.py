3a09f85855300462e351daad5af7c29f98cd9dc2
Brendon <hall.brendonp@gmail.com>
2023-11-26 14:47:19 -0600

    Change Activity

--- HallConnections/CRM/admin.py
+++ HallConnections/CRM/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import *
 
+class ActivityInLine(admin.TabularInline):
+    model = Activity
+    extra = 0
+    min_num = 1
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = [
-        "first_name",
-        "last_name"
+    inlines = [
+        ActivityInLine
     ]
+    pass
+    # list_display = [
+    #     "first_name",
+    #     "last_name",
+    # ]
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    pass
--- HallConnections/CRM/models.py
+++ HallConnections/CRM/models.py
@@ -10,8 +10,42 @@ class Contact(models.Model):
     created_on = models.DateField(auto_now_add=True)
 
 class Change(models.Model):
-    user = models.ForeignKey(authModels.User, on_delete=models.RESTRICT)
+    user = models.ForeignKey(
+        authModels.User,
+        on_delete=models.RESTRICT
+    )
     created_on = models.DateTimeField(auto_created=True)
+
+class Activity(models.Model):
+    KIND_CALL = "Call"
+    KIND_EMAIL = "Email"
+    KIND_TEXT = "Text"
+
+    KIND_CHOICES = [
+        (KIND_CALL, "Call"),
+        (KIND_EMAIL, "Email"),
+        (KIND_TEXT, "Text")
+    ]
+
+    contact = models.ForeignKey(
+        Contact,
+        on_delete=models.RESTRICT
+        )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+    kind = models.CharField(
+        max_length=10,
+        choices=KIND_CHOICES,
+        default=KIND_CALL
+        )
+    
+    text = models.TextField(
+        blank=True,
+        null=True
+    )
+    
+    
 
 
 
